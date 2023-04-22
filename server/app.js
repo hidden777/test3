71aec4c7a92d14f3f867358bf31b32888bdf96c2
Abhinav <sushensinha8@gmail.com>
2023-04-22 19:05:33 +0530

    fin

--- server/app.js
+++ server/app.js
@@ -13,14 +13,14 @@ app.get('/', (req, res)=>{
 app.listen('3000', ()=>{
     console.log('server is running at port 3000');
 });
-// newUser();
-// async function newUser(){
-//     try{
-//         const user = await User.create({name: "Kyle", email:"abcd123@gmail.com", phone:"9818723449", pass:"12345678"})
-//         await user.save();
-//         console.log(user);
-//     }
-//     catch (e){
-//         console.log(e.message);
-//     }
-// }
+newUser();
+async function newUser(){
+    try{
+        const user = await User.create({name: "Kyle", email:"abcd123@gmail.com", phone:"9818723449", pass:"12345678"})
+        await user.save();
+        console.log(user);
+    }
+    catch (e){
+        console.log(e.message);
+    }
+}
